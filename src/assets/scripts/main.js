b7485b8e08f9995a0ea0ea89049d36fd621d5cd0
NikSoftgrad <nik_k@softgrad.com>
2022-02-01 14:57:30 +0300

    bugfix "home page"

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,9 +5,9 @@ const fullScreenInfo = document.querySelector('.header__menu-nav');
 const iconMenu = document.querySelector('.header__line');
 if (iconMenu) {
    iconMenu.addEventListener("click", function (e) {
-      document.body.classList.toggle('_lock');
-      iconMenu.classList.toggle('_active');
-      fullScreenInfoRow.classList.toggle('_active');
-      fullScreenInfo.classList.toggle('_active');
+      document.body.classList.toggle('js-lock');
+      iconMenu.classList.toggle('js-is-active');
+      fullScreenInfoRow.classList.toggle('js-is-active');
+      fullScreenInfo.classList.toggle('js-is-active');
    });
 }
