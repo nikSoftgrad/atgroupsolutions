84b4dd959a233f5357f579006d89b7b6dbebbafd
NikSoftgrad <nik_k@softgrad.com>
2022-02-01 18:55:11 +0300

    edits "home page"

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,12 +3,18 @@
 const fullScreenInfoRow = document.querySelector('.main-content');
 const fullScreenInfo = document.querySelector('.header__menu-nav');
 const burgerMenu = document.querySelector('.header__burger-btn');
-const iconMenu = document.querySelector('.header__line');
+const burgerMenuClose = document.querySelector('.header__burger-close');
 if (burgerMenu) {
    burgerMenu.addEventListener("click", function (e) {
-      document.body.classList.toggle('js-lock');
-      iconMenu.classList.toggle('js-is-active');
-      fullScreenInfoRow.classList.toggle('js-is-active');
-      fullScreenInfo.classList.toggle('js-is-active');
+      document.body.classList.add('js-lock');
+      fullScreenInfoRow.classList.add('js-is-active');
+      fullScreenInfo.classList.add('js-is-active');
+   });
+}
+if (burgerMenuClose) {
+   burgerMenuClose.addEventListener("click", function (e) {
+      document.body.classList.remove('js-lock');
+      fullScreenInfoRow.classList.remove('js-is-active');
+      fullScreenInfo.classList.remove('js-is-active');
    });
 }
