62cd292142f12016c1c062b7c041add7d1a2ab12
NikSoftgrad <nik_k@softgrad.com>
2022-02-01 16:22:26 +0300

    error correction "home page"

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,9 +2,10 @@
 
 const fullScreenInfoRow = document.querySelector('.main-content');
 const fullScreenInfo = document.querySelector('.header__menu-nav');
+const burgerMenu = document.querySelector('.header__burger-btn');
 const iconMenu = document.querySelector('.header__line');
-if (iconMenu) {
-   iconMenu.addEventListener("click", function (e) {
+if (burgerMenu) {
+   burgerMenu.addEventListener("click", function (e) {
       document.body.classList.toggle('js-lock');
       iconMenu.classList.toggle('js-is-active');
       fullScreenInfoRow.classList.toggle('js-is-active');
